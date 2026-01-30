da8aa22b7f3685eb28dd5fe97489fd796c13871d
hardikmittal23 <106722937+hardikmittal23@users.noreply.github.com>
2026-01-30 17:26:53 +0530

    HOST

--- AIML_backend/settings.py
+++ AIML_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-diqrnrl#o1e=kxb*(8y&^)4gatwq9ckg5+l38mjs$wfwqob)of
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '*']
 
 
 # Application definition
