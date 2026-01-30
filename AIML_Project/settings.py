52a86a2179f0c58b9d1560085c0dfc555b0733ad
hardikmittal23 <106722937+hardikmittal23@users.noreply.github.com>
2026-01-30 17:06:11 +0530

    vercel

--- AIML_Project/settings.py
+++ AIML_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-diqrnrl#o1e=kxb*(8y&^)4gatwq9ckg5+l38mjs$wfwqob)of
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -119,3 +120,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
